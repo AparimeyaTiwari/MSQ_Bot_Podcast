5055221baa6bd72155ae27e010b056f9bc782274
AparimeyaOp <aparimeyatiwari@gmail.com>
2025-01-17 17:17:35 +0530

    model for summarization

--- ollama_code/yt_search_agent.py
+++ ollama_code/yt_search_agent.py
@@ -1,7 +1,49 @@
-from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api import YouTubeTranscriptApi as yta
+from ollama import chat
+from ollama import ChatResponse
+import ollama
 
-YouTubeTranscriptApi.get_transcript('Z1RJmh_OqeA&t=514s')
-transcript_list = YouTubeTranscriptApi.list_transcripts("Z1RJmh_OqeA&t=514s")
+link = input("Enter podcast link ")
+id = link[32:len(link)]
+data = yta.get_transcript(str(id))
+total_count = len(data)
 
-for transcript in transcript_list:
-    print(transcript.fetch())
+response_array = []
+cnt = 0
+op = ""
+quotient = total_count//5
+remainder = total_count%5
+print("total_count: ", total_count)
+print(quotient)
+print(remainder)
+
+for i in data:
+    op += "".join(i['text'])
+    op += " "
+    cnt += 1
+    print(cnt)
+    if(cnt % quotient == 0 and total_count - cnt != remainder):
+        print("yeeh")
+        response : ChatResponse = chat(model="my_sum_model",messages = [
+            {
+                'role':'user',
+                'content':f"{op}",
+            },
+        ])
+        response_array.append(response.message.content)
+        op = " "
+    if(total_count - cnt == remainder):
+        break
+
+final_op = ""
+for j in response_array:
+    final_op += j
+    final_op += " "
+
+response : ChatResponse = chat(model="my_sum_model",messages = [
+            {
+                'role':'user',
+                'content':f"final_op",
+            },
+])
+print(response.message.content)
