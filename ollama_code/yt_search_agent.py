715fc30af7dc585bb2786307c9994c6bb074d8e9
AparimeyaOp <aparimeyatiwari@gmail.com>
2025-01-17 12:35:09 +0530

    commit

--- ollama_code/yt_search_agent.py
+++ ollama_code/yt_search_agent.py
@@ -1,2 +1,7 @@
-import sys
-print(sys.path)
+from youtube_transcript_api import YouTubeTranscriptApi
+
+YouTubeTranscriptApi.get_transcript('Z1RJmh_OqeA&t=514s')
+transcript_list = YouTubeTranscriptApi.list_transcripts("Z1RJmh_OqeA&t=514s")
+
+for transcript in transcript_list:
+    print(transcript.fetch())
