0193be233ac7cbdc142c88fad2b153ae2de432ec
AparimeyaOp <aparimeyatiwari@gmail.com>
2025-01-17 12:20:32 +0530

    commit

--- extraction/input_resume/resume_parse.py
+++ extraction/input_resume/resume_parse.py
@@ -1,10 +1,12 @@
 import pymupdf
 
-doc = pymupdf.open("richa_resume.pdf")
-out = open("output_resume1.txt","wb")
 
-for page in doc:
-    text = page.get_text().encode("utf8")
-    out.write(text)
+def resume_parsing(file):
+    doc = pymupdf.open("richa_resume.pdf")
+    out = open("output_resume1.txt","wb")
 
-out.close()
+    for page in doc:
+        text = page.get_text().encode("utf8")
+        out.write(text)
+
+    out.close()
