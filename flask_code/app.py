e692e70b7c063cea9e8541e951aff36f93120b13
AparimeyaOp <aparimeyatiwari@gmail.com>
2025-01-20 16:45:43 +0530

    updated_flask

--- flask_code/app.py
+++ flask_code/app.py
@@ -1,23 +1,28 @@
 from flask import Flask, Response, render_template, request, url_for,redirect
+from werkzeug.utils import secure_filename
+import os
+
 
 app = Flask(__name__)
+app.config["UPLOAD_DIRECTORY"] = "uploads/"
 
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/<name>")
-def user(name):
-    return f"HELLO: {name}"
 
-@app.route("/page2",methods = ['POST'])
-def second_page():
-    html_data = request['resume']
-    return render_template("page2.html", html_data = html_data)
+@app.route("/podcast_question", methods=["POST"])
+def podcast_question():
+    file = request.files['file'] #files is an array of files, name
+    if file:
+        file.save(os.path.join(
+            app.config["UPLOAD_DIRECTORY"],
+            secure_filename(file.filename)
+        ))
+    else:
+        return "No file uploaded"
+    return redirect("/")
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user",name="ADMIN!!"))
 
 if __name__ == "__main__":
     app.run(debug=True)
