d4a2ee5a92311c090606fcca1096f8fdd27fbbdd
AparimeyaOp <aparimeyatiwari@gmail.com>
2025-01-22 22:31:29 +0530

    integration

--- flask_code/app.py
+++ flask_code/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, redirect,request
 from werkzeug.utils import secure_filename
 import os
-from ollama_code.resume_parse import resume_parsing
-from ollama_code.text_ner import ner_generator
-from ollama_code.theme_generation import theme
-from ollama_code.yt_search_agent import convert
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+from my_package.questions_gen import question_generator
+from my_package.resume_parse import resume_parsing
+from my_package.text_ner import ner_generator
+from my_package.theme_generation import theme
+from my_package.yt_search_agent import convert
+
+
 
 combined_data = []
 app = Flask(__name__)
@@ -18,19 +23,24 @@ def home():
 @app.route('/podcast_question', methods=['POST'])
 def podcast_question():
     my_array = []
-    guest = request.name
+    guest = request.form['guest_name']
     file = request.files['file'] #files is an array of files, name
-    theme_data = request.theme
-    youtube = request.yt
+    theme_data = request.form['theme']
+    youtube = request.form['yt']
     if file:
         file.save(os.path.join(
             app.config['UPLOADS_DIRECTORY'],
             secure_filename(file.filename)
         ))
-        resume_parsing(file)
-        
-        return 'Data submitted sucessfully!!!Please stand by while we generate the podcast questions'
-    return redirect('/process')
+        resume_parsing(f'uploads/{file.filename}')
+        file_name = os.path.splitext(file.filename)
+        final_name = file_name[0]+'.txt'
+        ner_generator(f'json/{final_name}',my_array)
+        theme(youtube,my_array)
+        question = question_generator(my_array)
+        convert(question,guest)
+        return 'Data submitted sucessfully!!!Please stand by while we generate the podcast questions' 
+    return redirect('/')
 
 
 if __name__ == "__main__":
