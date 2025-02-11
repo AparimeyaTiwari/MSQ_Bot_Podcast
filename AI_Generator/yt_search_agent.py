1e78f061831676b6f22c1412eda50e79415fdcf0
AparimeyaOp <aparimeyatiwari@gmail.com>
2025-02-11 20:26:38 +0530

    encoding error

--- AI_Generator/yt_search_agent.py
+++ AI_Generator/yt_search_agent.py
@@ -9,12 +9,12 @@ from AI_Generator.text_ner import ner_generator
 from AI_Generator.questions_gen import question_generator
 from AI_Generator.theme_generation import theme
 from pathlib import Path
-
+import os
 
 def convert(input,name):
     class PDF(FPDF):
         def header(self):
-            self.set_font('Helvetica','BI',30)
+            self.set_font("DejaVuSans","",30)
             self.cell(0,10,f'{name.upper()}',ln=1,align = 'C')
             self.ln(10)
 
@@ -24,12 +24,14 @@ def convert(input,name):
             self.cell(0,10,f'Page: {self.page_no()}',align='C')
 
         def add_chapter(self,txt):
-            self.set_font('times','',14)
+            self.set_font('DejaVuSans','',14)
             self.multi_cell(0,5,txt)
             self.ln(2)
 
 
     pdf =PDF()
+    path_font = os.path.join(os.path.dirname(__file__),"DejaVuSans.ttf")
+    pdf.add_font("DejaVuSans","",path_font,uni=True)
     pdf.add_page()
     pdf.set_auto_page_break(auto = True, margin=10)
     pdf.add_chapter(input)
